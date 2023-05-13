31371751f47e3ccd35d44ebfd7e56ff54db761c0
biox86 <biox86@gmail.com>
2023-05-13 22:41:12 +0200

    Disable seedbox purchase

--- sections/bonus/purchase.php
+++ sections/bonus/purchase.php
@@ -5,15 +5,16 @@ authorize();
 $label = $_REQUEST['label'];
 $bonus = new Gazelle\User\Bonus($Viewer);
 
-if ($label === 'collage-1') {
+if ($label === 'collage-1' || $label === 'aaa' ) {
     if (!$bonus->purchaseCollage($label)) {
         error('Could not purchase a personal collage slot due to lack of funds.');
     }
     header("Location: bonus.php?complete=$label");
 } elseif ($label === 'seedbox') {
-    if (!$bonus->unlockSeedbox()) {
-        error('Could not unlock the seedbox viewer. Either you have already unlocked it, or you lack the required bonus points.');
-    }
+    // if (!$bonus->unlockSeedbox()) {
+    //     error('Could not unlock the seedbox viewer. Either you have already unlocked it, or you lack the required bonus points.');
+    // }
+    error('Could not unlock the seedbox viewer. Seedbox purchase is currently unavailable.');
     header("Location: bonus.php?complete=$label");
 } else {
     error(403);
