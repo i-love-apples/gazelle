77e15734e322b8ba44ad93e12098e93239c0315a
voaii <voaii@proton.me>
2023-05-13 03:13:28 +0000

    cleanup migration

--- db/migrations/20230512204035_light_mono_stylesheet.php
+++ db/migrations/20230512204035_light_mono_stylesheet.php
@@ -7,14 +7,14 @@ final class LightMonoStylesheet extends AbstractMigration
 {
 public function down() {
     $this->table('stylesheets')->insert([
-        ['Name' => 'iAnon Light', 'Description' => 'iAnon Light by iAnon']
+        ['Name' => 'iAnon Light Mono', 'Description' => 'iAnon Light Mono by iAnon']
     ])->save();
 }
 
 public function up()
 {
     $this->table('stylesheets')->insert([
-        ['Name' => 'iAnon Light', 'Description' => 'iAnon Light by iAnon']
+        ['Name' => 'iAnon Light Mono', 'Description' => 'iAnon Light Mono by iAnon']
     ])->save();
 }
 }
